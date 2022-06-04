ce25ae0a928b73fd9c83e50a899c5e819b8a42b3
SilviaYembi <s.mik@hotmail.com>
2022-06-04 14:39:17 +1000

    Removing console log

--- src/components/Users/ListUsers.js
+++ src/components/Users/ListUsers.js
@@ -1,21 +1,16 @@
 import User from "./User";
-import style from "./ListUsers.module.css"
+import style from "./ListUsers.module.css";
 
 const ListUsers = (props) => {
-console.log('props.users ', props.users);
-    return (
-        <ul className={style['users-list']}>
-          {props.users.map((user) => (
-            <User
-              key={user.id}
-              id={user.id}
-            >
-              {user.userName} ({user.age} years old)
-            </User>
-          ))}
-        </ul>
-      );
-
-}
+  return (
+    <ul className={style["users-list"]}>
+      {props.users.map((user) => (
+        <User key={user.id} id={user.id}>
+          {user.userName} ({user.age} years old)
+        </User>
+      ))}
+    </ul>
+  );
+};
 
 export default ListUsers;
